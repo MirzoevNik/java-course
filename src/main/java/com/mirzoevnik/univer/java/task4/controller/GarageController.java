a8d0188d5555d9b32247b9d27a6268d9568f1af1
Nikita <mirzoevnik96@gmail.com>
2018-03-11 16:54:59 +0400

    task4: fill cars table

--- src/main/java/com/mirzoevnik/univer/java/task4/controller/GarageController.java
+++ src/main/java/com/mirzoevnik/univer/java/task4/controller/GarageController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Collections;
+
 /**
  * @author mirzoevnik
  */
@@ -34,9 +36,17 @@ public class GarageController {
         model.addAttribute("name", garage.getName());
         if (garage.getCars().isEmpty()) {
             model.addAttribute("count", "Garage is empty");
+            model.addAttribute("cars", Collections.EMPTY_LIST);
         } else {
             model.addAttribute("count", "Count of cars: " + garage.getCars().size());
+            model.addAttribute("cars", garage.getCars());
         }
         return "garage";
+    }
+
+    @GetMapping("/add-car")
+    public String addCar(Model model) {
+
+        return "add-car";
     }
 }
