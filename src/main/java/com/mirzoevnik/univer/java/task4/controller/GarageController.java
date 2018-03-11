da873ee8c3b91eed70fc10099f8822a6ef5a6d1c
Nikita <mirzoevnik96@gmail.com>
2018-03-11 19:44:22 +0400

    task4 remove, add-car & save

--- src/main/java/com/mirzoevnik/univer/java/task4/controller/GarageController.java
+++ src/main/java/com/mirzoevnik/univer/java/task4/controller/GarageController.java
@@ -1,11 +1,16 @@
 package com.mirzoevnik.univer.java.task4.controller;
 
+import com.mirzoevnik.univer.java.task3.domain.Car;
 import com.mirzoevnik.univer.java.task3.domain.Garage;
+import com.mirzoevnik.univer.java.task3.domain.Mark;
 import com.mirzoevnik.univer.java.task3.service.GarageService;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collections;
 
@@ -44,9 +49,42 @@ public class GarageController {
         return "garage";
     }
 
+    @PostMapping("/garage")
+    @SneakyThrows
+    public String save() {
+        garageService.save(garage);
+        return "redirect:/garage";
+    }
+
     @GetMapping("/add-car")
     public String addCar(Model model) {
-
+        Car car = new Car();
+        Mark mark = new Mark();
+        com.mirzoevnik.univer.java.task3.domain.Model modelDomain = new com.mirzoevnik.univer.java.task3.domain.Model();
+        modelDomain.setMark(mark);
+        car.setModel(modelDomain);
+        model.addAttribute("car", car);
         return "add-car";
+    }
+
+    @PostMapping("/add-car")
+    @SneakyThrows
+    public String addCarAction(@ModelAttribute Car car) {
+        garageService.addCar(garage, car);
+        return "redirect:/garage";
+    }
+
+    @GetMapping("/remove-car")
+    public String removeCar(Model model) {
+        Car car = new Car();
+        model.addAttribute("car", car);
+        return "remove-car";
+    }
+
+    @PostMapping("/remove-car")
+    @SneakyThrows
+    public String removeCarAction(@ModelAttribute Car car) {
+        garageService.removeCar(garage, car.getNumber());
+        return "redirect:/garage";
     }
 }
